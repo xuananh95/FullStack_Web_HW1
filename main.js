c73c09d86f3e60adff1e2c33f7c3e7e235c9a9e1
xuananh95 <xanh.trym95@gmail.com>
2022-06-30 14:54:06 +0700

    added comments

--- main.js
+++ main.js
@@ -1,20 +1,26 @@
+// Constructor Function
 function Car(_make, _speed) {
     this.make = _make;
     this.speed = _speed;
-    this.accelerate = function () {
-        this.speed += 10;
-        console.log(`Car ${this.make} speed: ${this.speed}`);
-    };
 }
 
+// Adding new method accelerate
+Car.prototype.accelerate = function () {
+    this.speed += 10;
+    console.log(`Car ${this.make} speed: ${this.speed}`);
+};
+
+// Adding new method brake
 Car.prototype.brake = function () {
     this.speed -= 5;
     console.log(`Car ${this.make} speed: ${this.speed}`);
 };
 
+// Initializing new objects
 const bmw = new Car("BMW", 120);
 const mercedes = new Car("Mercedes", 95);
 
+// Testing methods
 bmw.accelerate();
 bmw.accelerate();
 bmw.brake();
